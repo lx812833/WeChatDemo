4a141e312f6eb773d50d2a18664c1a172ca4fe9b
lx812833 <lx812833@163.com>
2021-03-31 23:45:38 +0800

    安装koa-logger

--- server/app.js
+++ server/app.js
@@ -1,10 +1,20 @@
-const Koa = require("koa")
+const koa = require("koa")
+const logger = require("koa-logger")
+const moment = require("moment")
 
-const app = new Koa()
+const app = new koa()
+app.use(logger())
 
 app.use(async (ctx) => {
     ctx.body = `<h1>hello node</h1>`
 })
+
+// 打印请求日志
+app.use(async (ctx, next) => {
+    await next();
+    const rt = ctx.response.get('X-Response-Time');
+    console.log(`执行时间：${ctx.method} ${ctx.url} - ${rt}`);
+});
 
 app.listen(3000, () => {
     console.log(`[Server] starting at port 3000`)
