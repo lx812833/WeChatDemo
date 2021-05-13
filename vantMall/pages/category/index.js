d7dd0c55822dffb5c4cc3c42a4a491f09f8afb57
lx812833 <lx812833@163.com>
2021-05-13 23:58:42 +0800

    商品列表数据延迟加载（调用组件方法）

--- vantMall/pages/category/index.js
+++ vantMall/pages/category/index.js
@@ -4,33 +4,40 @@ Page({
   data: {
     vtabs: [],
     activeTab: 0,
+    imageURL: "https://thirdwx.qlogo.cn/mmopen/vi_32/ZU6icabEmOxVYReclUpjq4aLRicQxX5s43UXtzzTp4ELDXNpJJr8ricicQqAdhB7j3YyJvqZcZDibiazIezBnvUTyguQ/132",
     goodsListMap: {}
   },
 
-  onLoad() {
-    getCategories().then(res => {
-      const vtabs = res.map(item => {
-        this.getGoodsListByCategory(item.id)
-        return ({ title: item.category_name, id: item.id })
-      })
+  async onLoad() {
+    let categories = await getCategories()
+    if(categories) {
+      let vtabs = []
+      for (let i = 0, len = categories.length; i < len; i++) {
+        const el = categories[i]
+        await this.getGoodsListByCategory(el.id)
+        vtabs.push({ title: el.category_name, id: el.id })
+      }
       this.setData({ vtabs })
-    })
+
+      // 调用组件方法
+      const vtabsComponent = this.selectComponent("#vtabs")
+      console.log("vtabsComponent", vtabsComponent, vtabsComponent.test(1))
+    }
   },
 
   // 根据商品分类id获取商品
   async getGoodsListByCategory(categoryId) {
     const data = {
       category_id: categoryId,
-      // page_size: 10,
-      // page_index: 1
+      page_size: 10,
+      page_index: 1
     }
     let result = await getGoods(data)
-    if(result) {
+    if (result) {
       this.setData({
         [`goodsListMap[${categoryId}]`]: result
       })
     }
-    console.log("商品列表", 123, this.data.goodsListMap)
   },
 
   onTabCLick(e) {
